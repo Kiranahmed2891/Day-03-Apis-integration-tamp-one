a558656040f21ab7804a59262d07b20e9ec82bca
Kiranahmed <148257482+Kiranahmed2891@users.noreply.github.com>
2025-01-22 00:38:57 +0500

    nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,11 @@
 // /** @type {import('next').NextConfig} */
-import dotenv from "dotenv"
+
 // const nextConfig = {};
 
 // export default nextConfig;
-dotenv.config()
+
 export const nextConfig = {
-  env: {
-    SHIPENGINE_API_KEY: process.env.SHIPENGINE_API_KEY,
-    SHIPENGINE_FIRST_COURIER: process.env.SHIPENGINE_FIRST_COURIER,
-    SHIPENGINE_SECOND_COURIER: process.env.SHIPENGINE_SECOND_COURIER,
-    SHIPENGINE_THIRD_COURIER: process.env.SHIPENGINE_THIRD_COURIER,
-    SHIPENGINE_FOURTH_COURIER: process.env.SHIPENGINE_FOURTH_COURIER,
-  },
+
     reactStrictMode: true,
     images: {
       domains: ['cdn.sanity.io'],
